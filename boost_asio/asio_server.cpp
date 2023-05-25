542b967c8adb5e37cbc75109d3796d8673fa070e
sean.lee <sean.lee@endoquestrobotics.com>
2023-05-25 10:42:53 +0900

    boost asio socket test

--- boost_asio/asio_server.cpp
+++ boost_asio/asio_server.cpp
@@ -5,91 +5,83 @@
 #include <iostream>
 #include <unordered_map>
 #include <memory>
+#include <future>
 
 
 using boost::asio::ip::tcp;
 
-class session;
 
-std::unordered_map<int, session> g_clients;
-std::atomic_int g_client_id = 0;
-
-
-
-class session
+class wall
 {
-	int my_id;
-	tcp::socket socket_;
-	enum {max_length = 1024};
-	char data_[max_length];
-
-
 	public:
-	session(): socket_(nullptr)
-	{
-		std::cout<< "session creation error"<<std::endl;
-	}
+		static constexpr std::size_t buf_size = 16* 1024u;
+		wall(){
+		
+		}
 
-	session(tcp::socket socket, int id) : socket_(std::move(socket)), my_id(id)
-	{
-		do_read();
-	}
-
-	void do_read()
-	{
-		socket_.async_read_some(boost::asio::buffer(data_, max_length), 
-		[this](boost::system::error_code ec, std::size_t length)
+		void accept_callback(boost::system::error_code ec, tcp::socket& socket, tcp::acceptor& my_acceptor)
 		{
-			if(ec){
-				std::cout<< "Disconnect client [ " << my_id << " ] "<<std::endl;
-				g_clients.erase(my_id); 
-			}
-			else{
-				data_[length] = 0;
-				std::cout<< "client [" << my_id << " ]: "<< data_ << std::endl;
-				g_clients[my_id].do_write(length);
-			}
-		});
-	}
+			//int new_id = g_client_id++;
+			//std::cout<< " New Client [ " << new_id << " ] connected"<<std::endl;
+			//g_clients.try_emplace(new_id, std::move(socket), new_id); // 왜 std::move ?
 
-	void do_write(std::size_t length)
-	{
-		boost::asio::async_write(socket_, boost::asio::buffer(data_,length),
-		[this](boost::system::error_code ec, std::size_t/*length*/)
-		{
-			if(!ec) g_clients[my_id].do_read();
-			else g_clients.erase(my_id);
-		} );
-	}
+			my_acceptor.async_accept([&my_acceptor,this](boost::system::error_code ec, tcp::socket socket){
+				socket.write_some(boost::asio::buffer(_contents));
+				accept_callback(ec, socket, my_acceptor);
+			});
+		}
+
+		bool post(std::string c, const std::string port){
+			std::cout<<"post"<<std::endl;
+			_contents = c;
+			boost::asio::ip::tcp::acceptor my_acceptor{_ioctx, tcp::endpoint(tcp::v4(), std::stoi(port))};
+
+			if(_future.valid()){
+				return false;
+			}
+			_future = std::async(std::launch::async, [&](){
+			my_acceptor.async_accept([&my_acceptor, this](boost::system::error_code ec, tcp::socket socket){
+				socket.write_some(boost::asio::buffer(_contents));
+				accept_callback(ec, socket, my_acceptor);
+				});
+				_ioctx.run();
+			});
+
+			while(true){
+				if(_future.wait_for(std::chrono::seconds(1)) == std::future_status::ready){
+					return true;
+				}
+			}
+
+			return true;
+		}
+	
+	private:
+		boost::asio::io_context _ioctx; // hw 정보를 읽어옴
+		//boost::asio::ip::tcp::socket _socket;
+		std::string _contents;
+		std::string _port;
+		std::future<void> _future;
+
 };
-
-void accept_callback(boost::system::error_code ec, tcp::socket& socket, tcp::acceptor& my_acceptor)
-{
-	int new_id = g_client_id++;
-	std::cout<< " New Client [ " << new_id << " ] connected"<<std::endl;
-
-	g_clients.try_emplace(new_id, std::move(socket), new_id);
-
-	my_acceptor.async_accept([&my_acceptor](boost::system::error_code ec, tcp::socket socket){
-		accept_callback(ec, socket, my_acceptor);
-	});
-}
-
 
 int main(int argc, char* argv[])
 {
 
-	boost::asio::io_context io_context;
-	
-	boost::asio::ip::tcp::acceptor my_acceptor{io_context, tcp::endpoint(tcp::v4(), 7777)};
-	
-	std::cout << "server started at port 7777" << std::endl;
+	wall w;
+	w.post("hello world", "10004");
 
-	my_acceptor.async_accept([&my_acceptor](boost::system::error_code ec, tcp::socket socket){
-		accept_callback(ec, socket, my_acceptor);
-	});
+	while(1){
 
-	io_context.run();
+	}
+
+	// boost::asio::io_context io_context;
+	// boost::asio::ip::tcp::acceptor my_acceptor{io_context, tcp::endpoint(tcp::v4(), 7777)};
+	// std::cout << "server started at port 7777" << std::endl;
+	// my_acceptor.async_accept([&my_acceptor](boost::system::error_code ec, tcp::socket socket){
+	// 	accept_callback(ec, socket, my_acceptor);
+	// });
+	// io_context.run();
 
 	
 
